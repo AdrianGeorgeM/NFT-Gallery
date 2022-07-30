0020fa5f2bf8ec402f9c659451ec3884d1fc4c30
Adriano <62570164+AdrianGeorgeM@users.noreply.github.com>
2022-07-30 15:04:15 +0100

    Added contract address button

--- nft-gallery/components/nftCard.jsx
+++ nft-gallery/components/nftCard.jsx
@@ -4,12 +4,25 @@ export const NFTCard = ({ varibles }) => {
 			<div>
 				<img src={varibles.media[0].gateway} />
 			</div>
-			<p>{varibles.title}</p>
-			<p>{varibles.id.tokenId.substr(varibles.id.tokenId.length - 4)}</p>
-			<p>
-				{`${varibles.contract.address.substr(0, 4)}`}...
-				{`${varibles.contract.address.substr(0, 4)}`}
-			</p>
+			<div>
+				<p>{varibles.title}</p>
+				<p>{varibles.id.tokenId.substr(varibles.id.tokenId.length - 4)}</p>
+				<p>
+					{`${varibles.contract.address.substr(0, 4)}`}...
+					{`${varibles.contract.address.substr(0, 4)}`}
+				</p>
+			</div>
+			<div>
+				<p>{varibles.description?.substr(0, 150)}</p>
+			</div>
+			<div>
+				<a
+					target={"_blank"}
+					href={`https://etherscan.io/token/${varibles.contract.address}`}
+				>
+					View on Etherscan
+				</a>
+			</div>
 		</div>
 	);
 };
