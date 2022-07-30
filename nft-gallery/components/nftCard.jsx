04ecd38a80347096ea747afc8a657c9306d04aae
Adriano <62570164+AdrianGeorgeM@users.noreply.github.com>
2022-07-30 14:34:39 +0100

    access the first index of the media object ,access the gateway property inside of it to get the image URL,and the contract address

--- nft-gallery/components/nftCard.jsx
+++ nft-gallery/components/nftCard.jsx
@@ -1,7 +1,15 @@
 export const NFTCard = ({ varibles }) => {
 	return (
 		<div>
+			<div>
+				<img src={varibles.media[0].gateway} />
+			</div>
 			<p>{varibles.title}</p>
+			<p>{varibles.id.tokenId.substr(varibles.id.tokenId.length - 4)}</p>
+			<p>
+				{`${varibles.contract.address.substr(0, 4)}`}...
+				{`${varibles.contract.address.substr(0, 4)}`}
+			</p>
 		</div>
 	);
 };
